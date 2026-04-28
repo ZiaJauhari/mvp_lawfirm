07175fd2fc8257865968074f3124a427a2b3df8b
Zia Jauhari <ziajauhari@gmail.com>
2026-04-28 16:47:03 +0800

    update8

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
     {{-- Favicon --}}
-    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}?v=1.0">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
