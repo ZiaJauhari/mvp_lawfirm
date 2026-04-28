f05978a51e7a6fb7c3095b2db14d4354c998312b
Zia Jauhari <ziajauhari@gmail.com>
2026-04-28 16:39:53 +0800

    update6

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
     {{-- Favicon --}}
-    <link rel="icon" type="image/png" href="{{ asset('storage/logo_lawfirm.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
